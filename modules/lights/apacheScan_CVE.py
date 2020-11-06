ee5d4851e6afd8f121ed11e4aea7772636c723aa
Ch3lLIST4 <herohoang74@gmail.com>
2020-11-06 22:47:04 +0700

    Update apacheScan_CVE.py

--- modules/lights/apacheScan_CVE.py
+++ modules/lights/apacheScan_CVE.py
@@ -5,6 +5,7 @@ import os
 import requests
 import re
 
+
 class bcolors:
     SUCCESS = '\033[92m'
     WARNING = '\033[93m'
@@ -13,11 +14,49 @@ class bcolors:
     ENDC = '\033[0m' 
     BOLD = '\033[1m'
 
+
 R = '\033[31m' # red
 G = '\033[32m' # green
 C = '\033[36m' # cyan
 W = '\033[0m'  # white
 Y = '\033[33m' # yellow
+
+
+def cve_details(target, cve_code):
+    target = 'https://www.cvedetails.com/cve/' + cve_code
+
+    user_agent = {
+        'User-Agent' : 'Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0'
+    }
+    # get soup
+    rqst = requests.get(target, headers=user_agent, verify=False)
+
+    sc = rqst.status_code
+    if sc == 200:
+            page = rqst.content
+            
+            soup = bs4.BeautifulSoup(page, 'lxml')
+
+            content = soup.text
+            if 'Unknown CVE ID' in content:
+                print(R + '[-]' + C + ' Unknown CVE ID' + W)
+                exit()
+            else:
+                pass
+
+            cvedetailssummary = soup.find("div", {"class": "cvedetailssummary"})
+            str = cvedetailssummary.text.strip()
+            str = str.replace('\t', '\n')
+            while '\n\n' in str:
+                str = str.replace('\n\n', '\n')
+            str = R + 'CVE Description : ' + W + str 
+            str = str + '\n' + R + 'CVE Details Url : ' + W + target
+            str = str.replace('Last Update Date :', R + 'Last Update Date :' + W)
+            str = str.replace('Publish Date :', R + 'Publish Date :' + W)
+            print(str)
+    else:
+        print('\n' + R + '[-]' + C + ' Could not found  ' + W + '\n')
+
 
 def checkVulns(target, output, data):
     result = {}
@@ -70,8 +109,15 @@ def checkVulns(target, output, data):
             print(G + '\n[+]' + C + ' Apache ' + bcolors.NEUTRAL + bcolors.BOLD + apache_version + bcolors.ENDC + ' is vulnerable to the following:' + '\n')
             print(bcolors.FAIL + "".join(vulns))
         else:
-            print(bcolors.FAIL + '\n[-]' + ' I am sorry but we could\'t find any vulnerabilities in our database for ' + bcolors.NEUTRAL + bcolors.BOLD + 'Apache ' + apache_version + bcolors.ENDC + '.')
+            print(bcolors.FAIL + '\n[-]' + ' We could\'t find any vulnerabilities in our database for ' + bcolors.NEUTRAL + bcolors.BOLD + 'Apache ' + apache_version + bcolors.ENDC + '.')
         
+        # cve details
+        for cve_code in vulns_rp:
+            print(G + '\n[+]' + C + cve_code + W)
+            print('-'*60)
+            cve_details(target, cve_code)
+            print('-'*60)
+
         if output != 'None':
             result['CVE-ID'] = vulns_rp
         
