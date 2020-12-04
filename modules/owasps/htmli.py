c3c524776880314615274b1350c9f7a8d98e91a3
Ch3lLIST4 <herohoang74@gmail.com>
2020-12-04 23:18:16 +0700

    Update htmli.py

--- modules/owasps/htmli.py
+++ modules/owasps/htmli.py
@@ -112,7 +112,7 @@ def scan_htmli(url, value_forms_malforms, htmli_data):
                 print(R + f"[-] HTMLi Detected on {url}" + W)
                 print(R + "[-]" + C + " Form details:" + W)
                 pprint(form_details.strip())
-                htmli_data.append(f"HTML Injection Detected on {url} | Form details: {form_details.strip() + "\n"}")
+                htmli_data.append(f"HTML Injection Detected on {url} | Form details: {form_details.strip() + '\n'}")
                 print(W)
                 value_forms_malforms[1] = value_forms_malforms[1] + 1
                 is_vulnerable = True
